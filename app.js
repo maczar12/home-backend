682fd5b9f6d92bc8295fe46f5e24e484d4efcce0
Przemek Macznik <przemek.macznik@gmail.com>
2019-01-20 16:19:58 +0100

    fix port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000
-const ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || 'localhost'
+const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
+const ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
