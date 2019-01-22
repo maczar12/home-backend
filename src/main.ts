e025e2d8a66c9abb8f0f18677e76e4b4c63e63ea
Przemek Macznik <przemek.macznik@gmail.com>
2019-01-22 20:49:06 +0100

    port ip

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8080, '0.0.0.0');
+  const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+  const ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+  await app.listen(port, ip);
 }
 bootstrap();
