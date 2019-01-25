833f65eca40057da15e95a16d98f7114b40c9f43
Przemyslaw Macznik <przemyslaw.macznik@codeconcept.pl>
2019-01-25 11:54:46 +0100

    46546

--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -15,9 +15,9 @@ export class CronService {
       this.vacationService.findAll().then(vacationOffer => {
         console.log('----vacationOffer', vacationOffer);
         this.vacationService.addPrice({
-          price: '11.11',
-          date: '2222',
-          vacationOffer: '5c48ac282365dc1930fef354',
+          price: '99.11',
+          date: '999',
+          vacationOffer: vacationOffer[0]._id,
         });
 
       });
